ed3c8827bd33e07bd30a1be220b6c5841ef4b808
6272 <6272user@6272s-Mac-mini-16.local>
2016-09-29 11:49:16 +0700

    swipe image and add text

---
+++ app/src/main/java/com/egco428/imageslider/CustomAdapter.java
@@ -0,0 +1,67 @@
+package com.egco428.imageslider;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by 6272user on 9/29/2016 AD.
+ */
+
+
+public class CustomAdapter extends PagerAdapter{
+
+    Context context;
+    int[] imageId = {R.drawable.image1, R.drawable.image2, R.drawable.image3, R.drawable.image4, R.drawable.image5};
+    String[] imageName = {"image1","image2","image3","image4","image5"};
+
+    public CustomAdapter(Context context){
+        this.context = context;
+
+    }
+
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        // TODO Auto-generated method stub
+
+        LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+
+        View viewItem = inflater.inflate(R.layout.image_item, container, false);
+        ImageView imageView = (ImageView) viewItem.findViewById(R.id.imageView);
+        imageView.setImageResource(imageId[position]);
+
+        TextView textView1 = (TextView) viewItem.findViewById(R.id.textView1);
+        textView1.setText(imageName[position]);
+        ((ViewPager)container).addView(viewItem);
+
+        return viewItem;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return imageId.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        // TODO Auto-generated method stub
+
+        return view == ((View)object);
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        // TODO Auto-generated method stub
+        ((ViewPager) container).removeView((View) object);
+    }
+
+}
---
+++ app/src/main/java/com/egco428/imageslider/MainActivity.java
@@ -0,0 +1,22 @@
+package com.egco428.imageslider;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        PagerAdapter adapter = new CustomAdapter(MainActivity.this);
+        viewPager.setAdapter(adapter);
+    }
+
+
+}
